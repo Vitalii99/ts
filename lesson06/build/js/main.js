80cd925ec149967dde81b6928f63094281e1312e
Vitalii99 <vitalykdc4704ct@gmail.com>
2023-08-21 00:08:42 +0300

    start lesson07

--- lesson06/build/js/main.js
+++ lesson06/build/js/main.js
@@ -1,29 +1,107 @@
 "use strict";
-// convert to more or less specific
-let a = 'hello';
-let b = a; // less specific type
-let c = a; // more specific
-let d = "world";
-let e = "world";
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b;
-};
-let myVal = addOrConcat(2, 2, 'concat');
-// be careful!!! TS sees no problem - but a string is returned
-let naxtVal = addOrConcat(2, 2, 'concat');
-// 10 as string
-10;
-// THE DOM
-const img = document.getElementById('img');
-const myImg = document.getElementById('#img');
-const nextImg = document.getElementById('#img');
-// const img1 = document.querySelector('#myId')
-img.src;
-myImg.src;
-nextImg.src;
-const img1 = document.getElementById('img1');
-const myImg1 = document.getElementById('#img1');
-img.src;
-myImg1.src;
+// class Coder {
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
+//     constructor(
+//         name: string,
+//         music: string,
+//         age: number,
+//         lang: string
+//     ) {
+//         this.name = name
+//         this.music = music
+//         this.age = age
+//         this.lang = lang
+//     }
+// }
+// МОДИФІКАТОРИ ВИДИМОСТІ
+class Coder1 {
+    // не ініціалізуєм одразу утримуватися від цього початківцям
+    constructor(name, music, age, lang = "Typescript") {
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+        this.name = name;
+        this.music = music;
+        this.age = age;
+        this.lang = lang;
+    }
+    getAge() {
+        return `Hello i'm ${this.age}`;
+    }
+}
+// const Vitalii = new Coder1("Vitalii", "Rock", 47, "Typescript")
+const Vitalii = new Coder1("Vitalii", "Rock", 47);
+console.log(Vitalii.getAge());
+// console.log(Vitalii.age)
+// console.log(Vitalii.lang)
+class WebDev extends Coder1 {
+    constructor(computer, name, music, age) {
+        super(name, music, age);
+        this.computer = computer;
+        this.computer = computer;
+    }
+    getLang() {
+        return `I write ${this.lang}`;
+    }
+}
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 33);
+console.log(Sara.getLang());
+class Guitarist {
+    constructor(name, instrument) {
+        this.name = name;
+        this.instrument = instrument;
+    }
+    play(action) {
+        return `${this.name} ${action} the ${this.instrument}`;
+    }
+}
+const Page = new Guitarist("Jimmy", "guitar");
+console.log(Page.play("strums"));
+//////////////////////////////////////////////
+class Peeps {
+    static getCount() {
+        return Peeps.count;
+    }
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+        this.id = ++Peeps.count;
+    }
+}
+Peeps.count = 0;
+const John = new Peeps("John");
+const Steve = new Peeps("Steve");
+const Amy = new Peeps("Amy");
+console.log(Amy.id);
+console.log(Steve.id);
+console.log(John.id);
+console.log(Peeps.count);
+//////////////////////////////////////////////
+class Bands {
+    constructor() {
+        this.dataState = [];
+    }
+    get data() {
+        return this.dataState;
+    }
+    set data(value) {
+        if (Array.isArray(value) && value.every(el => typeof el === "string")) {
+            this.dataState = value;
+            return;
+        }
+        else
+            throw new Error("Param is not array of strings");
+    }
+}
+const MyBands = new Bands();
+MyBands.data = ["Neil Youngs", "Led Zep"];
+console.log(MyBands.data);
+MyBands.data = [...MyBands.data, "ZZ Top"];
+console.log(MyBands.data);
+// MyBands.data="Van Halen" not array
+MyBands.data = ["Van Halen", 5150];
+console.log(MyBands.data);
--- lesson06/src/main.ts
+++ lesson06/src/main.ts
@@ -1,3 +1,148 @@
-class Coder {
-    
+// class Coder {
+//     name: string
+//     music: string
+//     age: number
+//     lang: string
+
+//     constructor(
+//         name: string,
+//         music: string,
+//         age: number,
+//         lang: string
+//     ) {
+//         this.name = name
+//         this.music = music
+//         this.age = age
+//         this.lang = lang
+//     }
+
+// }
+// МОДИФІКАТОРИ ВИДИМОСТІ
+class Coder1 {
+
+    secondLang!: string
+    // не ініціалізуєм одразу утримуватися від цього початківцям
+
+    constructor(
+        public readonly name: string,
+        public music: string,
+        private age: number,
+        protected lang: string = "Typescript"
+    ) {
+        this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+    }
+    public getAge() {
+        return `Hello i'm ${this.age}`
+    }
 }
+
+// const Vitalii = new Coder1("Vitalii", "Rock", 47, "Typescript")
+const Vitalii = new Coder1("Vitalii", "Rock", 47)
+
+console.log(Vitalii.getAge())
+// console.log(Vitalii.age)
+// console.log(Vitalii.lang)
+
+class WebDev extends Coder1 {
+    constructor(
+        public computer: string,
+        name: string,
+        music: string,
+        age: number,
+    ) {
+        super(name, music, age)
+        this.computer = computer
+    }
+    public getLang() {
+        return `I write ${this.lang}`
+    }
+}
+
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 33)
+console.log(Sara.getLang())
+// console.log(Sara.age)
+// console.log(Sara.lang)
+
+
+//////////////////////////////////////////////
+
+interface Musician {
+    name: string
+    instrument: string
+    play(action: string): string
+}
+
+class Guitarist implements Musician {
+    name: string
+    instrument: string
+
+    constructor(name: string, instrument: string) {
+        this.name = name
+        this.instrument = instrument
+    }
+    play(action: string): string {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist("Jimmy", "guitar")
+console.log(Page.play("strums"))
+
+//////////////////////////////////////////////
+
+class Peeps {
+    static count: number = 0
+
+    static getCount(): number {
+        return Peeps.count
+    }
+
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        this.id = ++Peeps.count
+    }
+}
+
+const John = new Peeps("John")
+const Steve = new Peeps("Steve")
+const Amy = new Peeps("Amy")
+
+console.log(Amy.id)
+console.log(Steve.id)
+console.log(John.id)
+console.log(Peeps.count)
+
+//////////////////////////////////////////////
+
+class Bands {
+    private dataState: string[]
+
+    constructor() {
+        this.dataState = []
+    }
+
+    public get data(): string[] {
+        return this.dataState
+    }
+
+    public set data(value: string[]) {
+        if (Array.isArray(value) && value.every(el => typeof el === "string")) {
+            this.dataState = value
+            return
+        } else throw new Error("Param is not array of strings")
+    }
+}
+
+const MyBands = new Bands()
+MyBands.data = ["Neil Youngs", "Led Zep"]
+console.log(MyBands.data)
+MyBands.data=[...MyBands.data, "ZZ Top"]
+console.log(MyBands.data)
+// MyBands.data="Van Halen" not array
+MyBands.data=["Van Halen", 5150]
+console.log(MyBands.data)
